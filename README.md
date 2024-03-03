d250e1d2c78bb5efeada7f4776fe084c4a5c84e0
Ramosas <ramosasik@gmail.com>
2024-03-03 21:57:39 +0100

    Podstawa apki

---
+++ README.md
@@ -0,0 +1,17 @@
+# Aplikacja mobilna README
+
+Milestone 1
+
+- Stworzenie szkieletu aplikacji z wykorzystaniem narzędzia flutter umożliwiającej rejestrację/logowanie, przenoszenie do nowych okien aplikacji, podstawowe poruszanie się po aplikacji
+- Stworzenie aplikacji desktopowej w języku python do podstawowej obsługi czytnika nfc 
+
+Milestone 2
+
+- Dodanie do aplikacji mobilnej i desktopowej funkcji komunikacji za pomocą czytnika nfc
+- Stworzenie bazy danych do przechowywania informacji o uzytkownikach w firebase
+- Dodanie do aplikacji funkcji mikropłatności
+
+Milestone 3
+
+- Gotowy produkt aplikacji mobilnej na systemy android i ios umożliwiającej rejestrację użytkownika lub logowanie, zakup wejść na skatepark jednorazowych i karnetów na zajęcia.
+- Sprawdzanie ważności wejścia za pomocą aplikacji komputerowej używając czytnika nfc (lub za pomocą skanowania kodów qr)
---
+++ lib/main.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Kontrolery do zarządzania wprowadzanym tekstem
+    final TextEditingController loginController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Logowanie'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Ekran logowania'),
+              const SizedBox(height: 20),
+              TextField(
+                controller: loginController,
+                decoration: const InputDecoration(
+                  labelText: 'Login',
+                  border: OutlineInputBorder(),
+                  hintText: 'Wpisz swój login',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Hasło',
+                  border: OutlineInputBorder(),
+                  hintText: 'Wpisz swoje hasło',
+                ),
+                obscureText: true, // Ukrywa wpisywane hasło
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HelloScreen()),
+                  );
+                },
+                child: const Text('Zaloguj się'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const RegistrationScreen()),
+                  );
+                },
+                child: const Text('Nie masz konta? Zarejestruj się'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // Kontrolery do zarządzania wprowadzanym tekstem
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rejestracja'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(labelText: 'Imię'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Proszę wpisać imię';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _lastNameController,
+                decoration: const InputDecoration(labelText: 'Nazwisko'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Proszę wpisać nazwisko';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'E-mail'),
+                validator: (value) {
+                  if (value == null || value.isEmpty || !value.contains('@')) {
+                    return 'Proszę wpisać poprawny adres e-mail';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Hasło'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length < 6) {
+                    return 'Hasło musi mieć co najmniej 6 znaków';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _phoneNumberController,
+                decoration: const InputDecoration(labelText: 'Numer telefonu'),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length != 9) {
+                    return 'Proszę wpisać poprawny numer telefonu';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Tutaj możesz dodać logikę rejestracji (np. wysłanie danych do API)
+                  }
+                },
+                child: const Text('Zarejestruj się'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _phoneNumberController.dispose();
+    super.dispose();
+  }
+}
+
+class HelloScreen extends StatelessWidget {
+  const HelloScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Witaj'),
+      ),
+      body: Center(
+        child: const Text('Hello', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
