77133ddae0ff6ae70aae98665b92c1e93208c452
Michał Zarzycki <s184856@student.pg.edu.pl>
2024-03-15 23:03:42 +0000

    skrypt odpowiedzialny za utworzenie nowego wezla w bazie danych, zapis plikow typu .json w bazie oraz za update danych w bazie danych

---
+++ Skrypty do obslugi bazy firebase/zapisz.py
@@ -0,0 +1,30 @@
+import firebase_admin
+from firebase_admin import credentials, db
+import json
+
+# zapis do bazy danych
+
+def zapisz_do_bazy_danych():
+
+    # Odczyt danych z pliku JSON
+    with open('uzytkownicy.json', 'r') as file:
+        data = json.load(file)
+
+    # Ustawienie referencji do węzła w bazie danych
+    ref = db.reference('/uzytkownicy')  # Zmieniono ścieżkę do węzła
+
+    # Zapis danych do bazy danych
+    ref.update(data)  # Użyj metody update, aby utworzyć lub zaktualizować węzeł
+
+def dodaj_nowy_wezel():
+
+    # Odczyt danych z pliku JSON
+    with open('uzytkownicy.json', 'r') as file:
+        data = json.load(file)
+
+    # Ustawienie referencji do nowego węzła w bazie danych
+    ref = db.reference('/uzytkownicy')
+
+    # Dodanie danych do nowego węzła z automatycznym generowaniem unikalnego klucza
+    ref.push(data)
+
