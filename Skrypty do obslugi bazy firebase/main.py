4b6bd8acd60b33a3e30bd89ee61aef6b8a2db1d8
Michał Zarzycki <s184856@student.pg.edu.pl>
2024-04-10 21:14:40 +0000

    Aktualizacja przykładowego uruchomienia serwera i obsługa klasy serwera

--- Skrypty do obslugi bazy firebase/main.py
+++ Skrypty do obslugi bazy firebase/main.py
@@ -1,13 +1,6 @@
-from zapisz import *
-from czytanie import odczytaj_z_bazy_danych
+from serwer import Serwer
 
 if __name__ == "__main__":
-    # Inicjalizacja aplikacji Firebase
-    cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://test-bazy-danych-b89e6-default-rtdb.europe-west1.firebasedatabase.app/'
-    })
-
-    # dodaj_nowy_wezel()
-    # zapisz_do_bazy_danych()
-    odczytaj_z_bazy_danych()
+    bazka = Serwer()
+    bazka.odczytaj_z_bazy_danych()
+    bazka.zapisz_dane_do_jsona()
