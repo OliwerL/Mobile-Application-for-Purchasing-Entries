2f520482845db70297e2999f9adafb60922855f8
OliwerL <oliwer.lisek@wp.pl>
2024-05-24 12:21:39 +0200

    usuwanie konta

--- lib/more_options.dart
+++ lib/more_options.dart
@@ -4,10 +4,8 @@ import 'package:mhapp/konkakt.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mhapp/zmiana_danych.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-
 import 'logowanie.dart';
+
 class MoreOptionsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -53,16 +51,23 @@ class MoreOptionsScreen extends StatelessWidget {
               ListTile(
                 title: Text('Kontakt'),
                 onTap: () {
-                  onSelected(context, 3);  // Call onSelected for logout
+                  onSelected(context, 3);  // Call onSelected for contact
                 },
                 textColor: Colors.white,
               ),
               ListTile(
                 title: Text('FAQ'),
                 onTap: () {
-                  onSelected(context, 4);  // Call onSelected for logout
+                  onSelected(context, 4);  // Call onSelected for FAQ
                 },
                 textColor: Colors.white,
+              ),
+              ListTile(
+                title: Text('Usuń konto'),
+                onTap: () {
+                  onSelected(context, 5);  // Call onSelected for account deletion
+                },
+                textColor: Colors.red, // Highlighting the delete option in red
               ),
             ],
           ).toList(),
@@ -72,20 +77,19 @@ class MoreOptionsScreen extends StatelessWidget {
   }
 }
 
-void onSelected(BuildContext context, int item) async { // Make the function async
+void onSelected(BuildContext context, int item) async {
   switch (item) {
-    case 0: // Przejście do ustawień konta
+    case 0:
       navigateToAccountSettings(context);
       break;
-    case 1: // Option to open a webpage
-    // Convert your URL string to a Uri object
+    case 1:
       final Uri url = Uri.parse('https://rollmasters.pl');
-      await launchUrl(url); // Use launchUrl with the Uri object
+      await launchUrl(url);
       break;
-    case 2: // Logout case
-      await FirebaseAuth.instance.signOut(); // Ensure user is signed out
+    case 2:
+      await FirebaseAuth.instance.signOut();
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (context) => LoginScreen(), // Replace with your login screen class
+        builder: (context) => LoginScreen(),
       ));
       break;
     case 3:
@@ -97,8 +101,11 @@ void onSelected(BuildContext context, int item) async { // Make the function asy
       );
       break;
     case 4:
+    // Handle FAQ option
       break;
-  // Handle other cases as needed
+    case 5:
+      _showDeleteAccountDialog(context);
+      break;
   }
 }
 
@@ -113,9 +120,70 @@ void navigateToAccountSettings(BuildContext context) {
       ),
     );
   } else {
-    // Użytkownik nie jest zalogowany lub błąd przy uzyskiwaniu identyfikatora użytkownika
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text('Błąd: Nie udało się uzyskać identyfikatora użytkownika')),
+    );
+  }
+}
+
+void _showDeleteAccountDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Usuń konto'),
+        content: Text('Czy na pewno chcesz usunąć swoje konto? Tej operacji nie można cofnąć.'),
+        actions: [
+          TextButton(
+            child: Text('Anuluj'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: Text('Usuń', style: TextStyle(color: Colors.red)),
+            onPressed: () {
+              Navigator.of(context).pop();
+              _deleteAccount(context);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+void _deleteAccount(BuildContext context) async {
+  User? user = FirebaseAuth.instance.currentUser;
+
+  if (user != null) {
+    try {
+      await user.delete();
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => LoginScreen(),
+      ));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Twoje konto zostało pomyślnie usunięte.')),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'requires-recent-login') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Do usunięcia konta wymagana jest ponowna autoryzacja. Zaloguj się ponownie i spróbuj ponownie.'),
+          ),
+        );
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Wystąpił błąd podczas usuwania konta: ${e.message}')),
+        );
+      }
+    }
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Nie udało się uzyskać informacji o użytkowniku.')),
     );
   }
 }
